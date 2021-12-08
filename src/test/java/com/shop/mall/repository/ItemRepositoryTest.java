74b3bb6f539060f25dd380d2a5396c7f344514ca
K-MHoon <azmria117@gmail.com>
2021-12-08 14:18:40 +0900

    feat : QueryDsl Setting, Test

--- src/test/java/com/shop/mall/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/mall/repository/ItemRepositoryTest.java
@@ -1,18 +1,32 @@
 package com.shop.mall.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.NumberPath;
+import com.querydsl.core.types.dsl.StringPath;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.mall.constant.ItemSellStatus;
 import com.shop.mall.entity.Item;
+import com.shop.mall.entity.QItem;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.util.ObjectUtils;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.shop.mall.entity.QItem.item;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +36,9 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @Autowired
+    EntityManager em;
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -41,7 +58,7 @@ class ItemRepositoryTest {
     /**
      * 테스트 시작시 10개의 상품을 저장한다.
      */
-    @BeforeEach
+//    @BeforeEach
     public void createItemList() {
         for(int i=1; i<= 10; i++) {
             Item item = new Item();
@@ -50,6 +67,33 @@ class ItemRepositoryTest {
             item.setItemDetail("테스트 상품 상세 설명" + i);
             item.setItemSellStatus(ItemSellStatus.SELL);
             item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @BeforeEach
+    public void createItemListV2() {
+        for(int i=1; i<= 5; i++) {
+            Item item = new Item();
+            item.setItemName("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+
+        for(int i=6; i<= 10; i++) {
+            Item item = new Item();
+            item.setItemName("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNumber(0);
             item.setRegTime(LocalDateTime.now());
             item.setUpdateTime(LocalDateTime.now());
             Item savedItem = itemRepository.save(item);
@@ -99,6 +143,83 @@ class ItemRepositoryTest {
         for (Item item : itemList) {
             System.out.println("item.toString() = " + item.toString());
         }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = item;
+        List<Item> itemList = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL),
+                        qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc())
+                .fetch();
+        for (Item item : itemList) {
+            System.out.println("item.toString() = " + item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품 Querydsl 조회 테스트 2")
+    public void queryDslTest2() {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+        String itemDetail = "테스트 상품 상세 설명";
+        int price = 10003;
+        String itemSellStat = "SELL";
+
+        booleanBuilder.and(item.itemDetail.like("%" + itemDetail + "%"));
+        booleanBuilder.and(item.price.gt(price));
+
+        if(StringUtils.equals(itemSellStat, ItemSellStatus.SELL)) {
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println("itemPagingResult.getTotalElements() = " + itemPagingResult.getTotalElements());
+
+        List<Item> resultItemList = itemPagingResult.getContent();
+        for (Item resultItem : resultItemList) {
+            System.out.println("resultItem.toString() = " + resultItem.toString());
+        }
+    }
+
+    /**
+     * BooleanExpression을 활용한 동적 조회
+     */
+    @Test
+    @DisplayName("상품 Querydsl 조회 테스트 3")
+    public void queryDslTest3() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        String itemDetail = "테스트 상품 상세 설명";
+        Integer price = 10007;
+        ItemSellStatus itemSellStat = ItemSellStatus.SOLD_OUT;
+
+        List<Item> itemList = queryFactory.selectFrom(item)
+                .where(itemDetailLike(itemDetail),
+                        itemPriceGt(price),
+                        itemSellStatusEq(itemSellStat))
+                .offset(0)
+                .limit(5)
+                .fetch();
+
+        for (Item item1 : itemList) {
+            System.out.println("item1.toString() = " + item1.toString());
+        }
+    }
+
+    private BooleanExpression itemSellStatusEq(ItemSellStatus itemSellStat) {
+        return ObjectUtils.isEmpty(itemSellStat) ? null : item.itemSellStatus.eq(itemSellStat);
+    }
+
+    private BooleanExpression itemPriceGt(Integer price) {
+        return ObjectUtils.isEmpty(price) ? null : item.price.gt(price);
+    }
+
+    private BooleanExpression itemDetailLike(String text) {
+        return StringUtils.isEmpty(text) ? null : item.itemDetail.like("%" + text + "%");
     }
 
 
