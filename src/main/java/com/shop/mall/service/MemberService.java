e907aa1ee6c04b5954534c431138331d557e1332
K-MHoon <azmria117@gmail.com>
2021-12-13 13:37:53 +0900

    feat : Login/Logout 구현
    - 스프링 Security Config 설정
    - 로그인/로그아웃에 따른 타임리프 작동 변경
    - UserDetailsService 활용

--- src/main/java/com/shop/mall/service/MemberService.java
+++ src/main/java/com/shop/mall/service/MemberService.java
@@ -4,6 +4,10 @@ import com.shop.mall.entity.Member;
 import com.shop.mall.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 @Slf4j
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -26,5 +30,16 @@ public class MemberService {
         if(findMember != null) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
