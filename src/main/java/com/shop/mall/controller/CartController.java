ba3b441018999af89917cbaffa49f0e5ae3a19f7
K-MHoon <azmria117@gmail.com>
2021-12-21 17:10:10 +0900

    feat : 장바구니 상품 주문 및 장바구니 내용 삭제 기능 추가
    - 장바구니에 있는 물품 주문시, 장바구니 비우기
    - 다수 물품 주문하는 기능 추가

--- src/main/java/com/shop/mall/controller/CartController.java
+++ src/main/java/com/shop/mall/controller/CartController.java
@@ -2,6 +2,7 @@ package com.shop.mall.controller;
 
 import com.shop.mall.dto.CartDetailDto;
 import com.shop.mall.dto.CartItemDto;
+import com.shop.mall.dto.CartOrderDto;
 import com.shop.mall.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -73,5 +74,23 @@ public class CartController {
 
         cartService.deleteCartItem(cartItemId);
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+    }
+
+    @PostMapping("/cart/orders")
+    public @ResponseBody ResponseEntity orderCartItem(@RequestBody CartOrderDto cartOrderDto, Principal principal) {
+        List<CartOrderDto> cartOrderDtoList = cartOrderDto.getCartOrderDtoList();
+
+        if(cartOrderDtoList == null || cartOrderDtoList.size() == 0) {
+            return new ResponseEntity<String>("주문할 상품을 선택해주세요", HttpStatus.FORBIDDEN);
+        }
+
+        for (CartOrderDto cartOrder : cartOrderDtoList) {
+            if(!cartService.validateCartItem(cartOrder.getCartItemId(), principal.getName())) {
+                return new ResponseEntity<String>("주문 권한이 없습니다.", HttpStatus.FORBIDDEN);
+            }
+        }
+
+        Long orderId = cartService.orderCartItem(cartOrderDtoList, principal.getName());
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
     }
 }
