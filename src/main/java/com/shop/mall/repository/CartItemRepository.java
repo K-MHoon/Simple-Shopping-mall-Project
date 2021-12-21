9e12649998c658f9b72b34210f894885d66c902e
K-MHoon <azmria117@gmail.com>
2021-12-21 16:39:19 +0900

    feat : 장바구니 조회, 수량 수정, 삭제 추가
    - 장바구니에서 수량 변경시 update 되도록 설정(PatchMapping)
    - 장바구니에서 물품 삭제시 delete 되도록 설정(DeleteMapping)
    - 검증을 통해 동일 고객이 아닐 경우, 변경되지 않도록 수정

--- src/main/java/com/shop/mall/repository/CartItemRepository.java
+++ src/main/java/com/shop/mall/repository/CartItemRepository.java
@@ -1,10 +1,22 @@
 package com.shop.mall.repository;
 
+import com.shop.mall.dto.CartDetailDto;
 import com.shop.mall.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Optional<CartItem> findByCartIdAndItemId(Long cartId, Long itemId);
+
+    @Query("select new com.shop.mall.dto.CartDetailDto(ci.id, i.itemName, i.price, ci.count, im.imgUrl) " +
+            "from CartItem ci, ItemImg im " +
+            "join ci.item i " +
+            "where ci.cart.id = :cartId " +
+            "and im.item.id = ci.item.id " +
+            "and im.repimgYn = 'Y' " +
+            "order by ci.regTime desc")
+    List<CartDetailDto> findCartDetailDtoList(Long cartId);
 }
--- src/main/java/com/shop/mall/service/CartService.java
+++ src/main/java/com/shop/mall/service/CartService.java
@@ -1,5 +1,6 @@
 package com.shop.mall.service;
 
+import com.shop.mall.dto.CartDetailDto;
 import com.shop.mall.dto.CartItemDto;
 import com.shop.mall.entity.Cart;
 import com.shop.mall.entity.CartItem;
@@ -12,8 +13,11 @@ import com.shop.mall.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -43,5 +47,58 @@ public class CartService {
             cartItemRepository.save(cartItem);
             return cartItem.getId();
         }
+    }
+
+    @Transactional(readOnly = true)
+    public List<CartDetailDto> getCartList(String email) {
+        List<CartDetailDto> cartDetailDtoList = new ArrayList<>();
+
+        Member member = memberRepository.findByEmail(email).orElseThrow(EntityNotFoundException::new);
+        Cart cart = cartRepository.findByMemberId(member.getId()).orElse(null);
+        if(cart == null) {
+            return cartDetailDtoList;
+        }
+
+        cartDetailDtoList = cartItemRepository.findCartDetailDtoList(cart.getId());
+
+        return cartDetailDtoList;
+    }
+
+    /**
+     * 장바구니를 조작하는 회원이 현재 회원과 동일한지 확인
+     * @param cartItemId 장바구니 물품 ID
+     * @param email 접근한 회원 email
+     * @return
+     */
+    @Transactional(readOnly = true)
+    public boolean validateCartItem(Long cartItemId, String email) {
+        Member curMember = memberRepository.findByEmail(email).orElseThrow(EntityNotFoundException::new);
+        CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
+        Member savedMember = cartItem.getCart().getMember();
+
+        if(!StringUtils.equals(curMember.getEmail(), savedMember.getEmail())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 장바구니 상품의 수량을 업데이트하는 메서드
+     * @param cartItemId 장바구니 물품 ID
+     * @param count 갯수
+     */
+    public void updateCartItemCount(Long cartItemId, int count) {
+        CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
+        cartItem.updateCount(count);
+    }
+
+    /**
+     * 장바구니 상품을 제거하는 메서드
+     * @param cartItemId 장바구니 물품 ID
+     */
+    public void deleteCartItem(Long cartItemId) {
+        CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
+        cartItemRepository.delete(cartItem);
     }
 }
