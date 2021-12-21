1dfbf921382bbe591b6bc8dae2fac4af0e81dcf9
K-MHoon <azmria117@gmail.com>
2021-12-21 12:04:23 +0900

    feat : 구매이력 확인 기능 구현
    - 주문한 내역을 모두 확인 할 수 있는 화면 구현
    - batch_size 조절로 N+1 문제 해결

---
+++ src/main/java/com/shop/mall/dto/OrderHistDto.java
@@ -0,0 +1,28 @@
+package com.shop.mall.dto;
+
+import com.shop.mall.constant.OrderStatus;
+import com.shop.mall.entity.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderHistDto {
+    private Long orderId;
+    private String orderDate;
+    private OrderStatus orderStatus;
+    private List<OrderItemDto> orderItemDtoList = new ArrayList<>();
+    public void addOrderItemDto(OrderItemDto orderItemDto) {
+        orderItemDtoList.add(orderItemDto);
+    }
+
+    public OrderHistDto(Order order) {
+        this.orderId = order.getId();
+        this.orderDate = order.getOrderDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.orderStatus = order.getOrderStatus();
+    }
+}
--- src/main/java/com/shop/mall/service/OrderService.java
+++ src/main/java/com/shop/mall/service/OrderService.java
@@ -1,14 +1,17 @@
 package com.shop.mall.service;
 
 import com.shop.mall.dto.OrderDto;
-import com.shop.mall.entity.Item;
-import com.shop.mall.entity.Member;
-import com.shop.mall.entity.Order;
-import com.shop.mall.entity.OrderItem;
+import com.shop.mall.dto.OrderHistDto;
+import com.shop.mall.dto.OrderItemDto;
+import com.shop.mall.entity.*;
+import com.shop.mall.repository.ItemImgRepository;
 import com.shop.mall.repository.ItemRepository;
 import com.shop.mall.repository.MemberRepository;
 import com.shop.mall.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +27,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto, String email) {
         Item item = itemRepository.findById(orderDto.getItemId()).orElseThrow(EntityNotFoundException::new);
@@ -37,5 +41,25 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+        Long totalCount = orderRepository.countOrder(email);
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+        for (Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+            for (OrderItem orderItem : orderItems) {
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
     }
 }
